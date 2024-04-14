429c0e34f37f196a2d7df596a596eaaf97dc580f
Christian MANICKA <christngomacn@gmail.com>
2024-04-14 12:00:48 +0200

    Ajout de la bibliotheque Armadillo pour l'algebre lineaire

---
+++ arma/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2
+# Build directory: /media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests1")
---
+++ arma/tests1/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2/tests1
+# Build directory: /media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2/tests1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(smoke_test "/media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2/tests1/smoke_test")
+set_tests_properties(smoke_test PROPERTIES  _BACKTRACE_TRIPLES "/media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2/tests1/CMakeLists.txt;5;add_test;/media/diehlm/Data/Linux/Stage_CY/armadillo-12.8.2/tests1/CMakeLists.txt;0;")
