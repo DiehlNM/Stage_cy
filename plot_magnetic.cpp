a52ae4fbfd0a5decd5afee8e113a3379ff5b4bfc
Christian MANICKA <christngomacn@gmail.com>
2024-04-15 10:50:46 +0200

    Ajout des fichiers pour le plot

---
+++ plot_magnetic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <armadillo>
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+using namespace std;
+using namespace arma;
+
+void plot_magnetic_moments(const vec& mx, const vec& my, int Dim) {
+    // Prepare data for the quiver plot
+    std::vector<double> x, y, u, v;
+    for (int i = 0; i < Dim; i++) {
+        for (int j = 0; j < Dim; j++) {
+            x.push_back(j);
+            y.push_back(i);
+            u.push_back(mx(i * Dim + j)); // Convert linear index to 2D
+            v.push_back(my(i * Dim + j)); // Convert linear index to 2D
+        }
+    }
+
+    // Set up plot limits and labels
+    plt::quiver(x, y, u, v);
+    plt::xlim(-1, Dim);
+    plt::ylim(-1, Dim);
+    plt::title("Magnetic Moment Directions");
+    plt::show();
+    
+}
